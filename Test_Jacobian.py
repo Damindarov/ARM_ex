4aa7cfcd3c91a98287c4d16587116721805356b6
Damindarov <damin4@mail.ru>
2021-09-12 22:37:20 +0300

    Kekes - just for save

--- Test_Jacobian.py
+++ Test_Jacobian.py
@@ -1,4 +1,5 @@
 import time
+from std_msgs.msg import Float32MultiArray
 import numpy as np
 import sympy as sym
 from matrix import *
@@ -7,15 +8,33 @@ import plotly.express as px
 import plotly.io as pio
 import plotly.graph_objects as go
 from math import atan2, sqrt
+import rospy
+from std_msgs.msg import String
+
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
 import pandas
 a1, a2, a3, a4 = 0.36, 0.42, 0.4, 0.126
 pi = np.pi
 pi_sym = sym.pi
+#!/usr/bin/env python
+# license removed for brevity
 
-def weighted_pseudo(q0, p_goal, weights=[10, 30, 25, 20, 90, 42, 57], time_step=0.1, max_iteration=3000,
-                    accuracy=0.001):
+def talker(q1, q2, q3, q4, q5, q6, q7):
+    pub_p = rospy.Publisher('lefttop_point', Float32MultiArray, queue_size=1)
+    rospy.init_node('talker', anonymous=True)
+    # rate = rospy.Rate(10)
+
+    array = [q1, q2, q3, q4, q5, q6, q7]
+    left_top = Float32MultiArray(data=array)
+    rospy.loginfo(left_top)
+    pub_p.publish(left_top)
+    # rate.sleep()
+
+
+
+def weighted_pseudo(q0, p_goal, weights=[10, 30, 25, 20, 90, 42, 57], time_step=0.1, max_iteration=300,
+                    accuracy=0.01):
     assert np.linalg.norm(p_goal) < 0.85 * np.sum([a1, a2, a3, a4]), "Robot Length constraint violated"
     q_n0 = q0
     p = FK(q_n0)[:3, -1]
@@ -224,42 +243,6 @@ def get_cnfs(method_fun, q0=np.deg2rad([0, 30, 0, -20, 0, 45, 0]), kwargs=dict()
     plot_robots(rob_cnfs, traj_x=x, traj_y=y, traj_z=z)
 
 
-def null_space_method(q0, p_goal, weights=[1, 3, 1, 2, 9, 4, 5], time_step=0.01, max_iteration=3000, accuracy=0.01):
-    assert np.linalg.norm(p_goal[:3]) <= 0.85 * np.sum([a1, a2, a3, a4]), "Robot Length constraint violated"
-    q_n0 = q0
-    p = FK(q_n0)[:3, -1]
-    t_dot = p_goal[:3] - p
-    H1 = [0, 0, 0, 0, 0, 0, 0]
-    e = np.linalg.norm(t_dot)
-    Tt = np.block([np.eye(3), np.zeros((3, 3))])
-    q_n1 = q_n0
-    δt = time_step
-    q_dot_0 = [4, 9, 1, 7, 9, 5, 1]  # [ 0.4, 0.9, 0.1, 0.7, 0.9, 0.5, 0.22]
-    i = 0
-    start_time = time.time()
-    while True:
-        if (e < accuracy):
-            break
-        fk = FK(q_n0)
-        rx, ry, rz = euler_angles(fk[:3, :3])
-        p = np.hstack([fk[:3, -1], [rx, ry, rz]])  # current position and orientation
-        t_dot = p_goal[:3] - p[:3]
-        e = np.linalg.norm(t_dot)
-        w_inv = np.linalg.inv(np.diag(weights))
-        Jt = np.dot(Tt, jacobian(q_n0))
-        j_hash = w_inv @ Jt.T @ np.linalg.inv(Jt @ w_inv @ Jt.T)
-        q_dot = (j_hash @ t_dot) + (np.eye(7) - (j_hash @ Jt)) @ q_dot_0
-        q_n1 = q_n0 + (δt * q_dot)
-        q_n0 = q_n1
-        i += 1
-        if (i > max_iteration):
-            print("No convergence")
-            break
-
-    end_time = time.time()
-    print(f"to {np.round(p_goal, 2)} :: time taken {np.round(end_time - start_time, 4)} seconds\n")
-
-    return np.mod(q_n1, 2 * np.pi)
 
 
 get_cnfs(method_fun=weighted_pseudo, q0=np.deg2rad([35,-20,10,20,0,60,0]))
