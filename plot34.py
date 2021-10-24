14ae76f7d95a727c37d6d97439e46c98944ed258
Damindarov <damin4@mail.ru>
2021-10-24 15:19:15 +0300

    Draw_workspace - file for drawing workspace using data from csvWriter

---
+++ plot34.py
@@ -0,0 +1,281 @@
+import numpy as np
+import sympy as sym
+import plotly.express as px
+import plotly.io as pio
+import plotly.graph_objects as go
+import time
+import numpy as np
+import pandas as pd
+import sympy as sym
+sym.init_printing()
+from math import atan2, sqrt
+import math
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+a1, a2, a3, a4 = 0.08, 0, 0.39, 0.32
+def Rx(q):
+  T = np.array([[1,         0,          0, 0],
+                [0, np.cos(q), -np.sin(q), 0],
+                [0, np.sin(q),  np.cos(q), 0],
+                [0,         0,          0, 1]], dtype=float)
+  return T
+
+def Ry(q):
+  T = np.array([[ np.cos(q), 0, np.sin(q), 0],
+                [         0, 1,         0, 0],
+                [-np.sin(q), 0, np.cos(q), 0],
+                [         0, 0,         0, 1]], dtype=float)
+  return T
+
+def Rz(q):
+  T = np.array([[np.cos(q), -np.sin(q), 0, 0],
+                [np.sin(q),  np.cos(q), 0, 0],
+                [        0,          0, 1, 0],
+                [        0,          0, 0, 1]], dtype=float)
+  return T
+
+
+def Rx_sym(q):
+  return sym.Matrix(
+      [[1, 0, 0, 0],
+        [0, sym.cos(q), -sym.sin(q), 0],
+        [0, sym.sin(q), sym.cos(q), 0],
+        [0, 0, 0, 1]]
+  )
+
+def Ry_sym(q):
+  return sym.Matrix(
+      [[sym.cos(q), 0, sym.sin(q), 0],
+        [0, 1, 0, 0],
+        [-sym.sin(q), 0, sym.cos(q), 0],
+        [0, 0, 0, 1]]
+  )
+
+def Rz_sym(q):
+  return sym.Matrix(
+      [[sym.cos(q), -sym.sin(q), 0, 0],
+        [sym.sin(q), sym.cos(q), 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]]
+  )
+
+def d_Rx(q):
+  T = np.array([[0,          0,          0, 0],
+                [0, -np.sin(q), -np.cos(q), 0],
+                [0,  np.cos(q), -np.sin(q), 0],
+                [0,          0,          0, 0]], dtype=float)
+  return T
+
+def d_Ry(q):
+  T = np.array([[-np.sin(q), 0,  np.cos(q), 0],
+                [         0, 0,          0, 0],
+                [-np.cos(q), 0, -np.sin(q), 0],
+                [         0, 0,          0, 0]], dtype=float)
+  return T
+
+def d_Rz(q):
+  T = np.array([[-np.sin(q), -np.cos(q), 0, 0],
+                [ np.cos(q), -np.sin(q), 0, 0],
+                [         0,          0, 0, 0],
+                [         0,          0, 0, 0]], dtype=float)
+  return T
+
+
+def d_Rx_sym(q):
+  return sym.Matrix(
+      [[0, 0, 0, 0],
+        [0, -sym.sin(q), -sym.cos(q), 0],
+        [0, sym.cos(q), -sym.sin(q), 0],
+        [0, 0, 0, 0]]
+  )
+
+def d_Ry_sym(q):
+  return sym.Matrix(
+      [[-sym.sin(q), 0, sym.cos(q), 0],
+        [0, 0, 0, 0],
+        [-sym.cos(q), 0, -sym.sin(q), 0],
+        [0, 0, 0, 0]]
+  )
+
+def d_Rz_sym(q):
+  return sym.Matrix(
+      [[-sym.sin(q), -sym.cos(q), 0, 0],
+        [sym.cos(q), -sym.sin(q), 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]])
+
+def Tx(x):
+  T = np.array([[1, 0, 0, x],
+                [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]], dtype=float)
+  return T
+
+def Ty(y):
+  T = np.array([[1, 0, 0, 0],
+                [0, 1, 0, y],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]], dtype=float)
+  return T
+
+def Tz(z):
+  T = np.array([[1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, z],
+                [0, 0, 0, 1]], dtype=float)
+  return T
+
+def Tx_sym(s):
+  return sym.Matrix(
+      [[1, 0, 0, s],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]]
+  )
+
+def Ty_sym(s):
+  return sym.Matrix(
+      [[1, 0, 0, 0],
+        [0, 1, 0, s],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]]
+  )
+
+def Tz_sym(s):
+  return sym.Matrix(
+      [[1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, s],
+        [0, 0, 0, 1]]
+  )
+
+def d_Tx(x):
+  T = np.array([[0, 0, 0, 1],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]], dtype=float)
+  return T
+
+def d_Ty(y):
+  T = np.array([[0, 0, 0, 0],
+                [0, 0, 0, 1],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]], dtype=float)
+  return T
+
+def d_Tz(z):
+  T = np.array([[0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 1],
+                [0, 0, 0, 0]], dtype=float)
+  return T
+
+
+def d_Tx_sym():
+  return sym.Matrix(
+      [[0, 0, 0, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]]
+  )
+
+def d_Ty_sym():
+  return sym.Matrix(
+      [[0, 0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]]
+  )
+
+def d_Tz_sym():
+  return sym.Matrix(
+      [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 0, 0]]
+  )
+def FK(q1,q2,q3,q4,q5):
+
+    T01 = np.eye(4)
+    T12 = Rx(q1) @ Tx(a1)  # Joint 1 to 2
+    T23 = Ry(q2) @ Tz(a2)  # np.eye(4)# Joint 2 to 3
+    T34 = Rz(q3) @ Rx(q4)  # Joint 3 to 4
+    T45 = Tz(a3)  # Rx(q3)                    # Joint 4 to 5
+    T56 = Tz(a4)  # Rz(q5) @ Tz(a3)           # Joint 5 to 6
+    T67 = np.eye(4)  # Rx(q6)                    # Joint 6 to 7
+    T7E = np.eye(4)  # Rz(q7) @ Tz(a4)           # Joint 7 to E
+
+
+
+
+    T02 = T01 @ T12
+    T03 = T01 @ T12 @ T23
+    T04 = T01 @ T12 @ T23 @ T34
+    T05 = T01 @ T12 @ T23 @ T34 @ T45
+    T06 = T01 @ T12 @ T23 @ T34 @ T45 @ T56
+    # T07 = T01 @ T12 @ T23 @ T34 @ T45 @ T56 @ T67
+    # T0E = T01 @ T12 @ T23 @ T34 @ T45 @ T56 @ T67 @ T7E
+
+    x_pos = T06[0, -1]
+    y_pos = T06[1, -1]
+    z_pos = T06[2, -1]
+    return x_pos, y_pos, z_pos
+
+
+data= pd.read_csv("q_pointcloud.csv")
+
+q1 = data['q1'].values.tolist()
+q2 = data['q2'].values.tolist()
+q3 = data['q3'].values.tolist()
+q4 = data['q4'].values.tolist()
+q5 = data['q5'].values.tolist()
+
+
+X = []
+Y= []
+Z = []
+for i in range(len(q5)):
+    x, y, z = FK(q1[i],q2[i],q3[i],q4[i],q5[i])
+    X.append(x)
+    Y.append(y)
+    Z.append(z)
+print(len(X))
+fig2 = go.Figure(data=[go.Scatter3d(x=X, y=Y, z=Z, mode='markers', marker=dict(
+        size=2,
+        color=z,                # set color to an array/list of desired values
+        colorscale='Viridis',   # choose a colorscale
+        opacity=0.8
+    ))])
+
+T01 = np.eye(4)@Rx(math.pi)
+T12 = Rx(0) @ Tx(a1)  # Joint 1 to 2
+T23 = Ry(0) @ Tz(a2)  # np.eye(4)# Joint 2 to 3
+T34 = Rz(0) @ Rx(0)  # Joint 3 to 4
+T45 = Tz(a3)  # Rx(q3)                    # Joint 4 to 5
+T56 = Tz(a4)  # Rz(q5) @ Tz(a3)           # Joint 5 to 6
+T67 = np.eye(4)  # Rx(q6)                    # Joint 6 to 7
+T7E = np.eye(4)  # Rz(q7) @ Tz(a4)           # Joint 7 to E
+
+T02 = T01 @ T12
+T03 = T01 @ T12 @ T23
+T04 = T01 @ T12 @ T23 @ T34
+T05 = T01 @ T12 @ T23 @ T34 @ T45
+T06 = T01 @ T12 @ T23 @ T34 @ T45 @ T56
+T07 = T01 @ T12 @ T23 @ T34 @ T45 @ T56 @ T67
+T0E = T01 @ T12 @ T23 @ T34 @ T45 @ T56 @ T67 @ T7E
+x_pos = [T01[0,-1], T02[0,-1], T03[0,-1], T04[0,-1], T05[0,-1], T06[0,-1], T07[0,-1], T0E[0,-1]]
+y_pos = [T01[1,-1], T02[1,-1], T03[1,-1], T04[1,-1], T05[1,-1], T06[1,-1], T07[1,-1], T0E[1,-1]]
+z_pos = [T01[2,-1], T02[2,-1], T03[2,-1], T04[2,-1], T05[2,-1], T06[2,-1], T07[2,-1], T0E[2,-1]]
+fig2.add_trace(go.Scatter3d(x=x_pos, y=y_pos, z=z_pos,
+                                   mode='lines+markers',marker_color='rgba(152, 13, 0, .8)',marker=dict(
+            color='LightSkyBlue',
+            size=8,
+            line=dict(
+                color="aliceblue",
+                width=8
+            )
+        )))
+fig2.update_yaxes(range=[-1, 1])
+fig2.update_xaxes(range=[-1, 1])
+fig2.show()
--- q_collector.py
+++ q_collector.py
@@ -11,6 +11,7 @@ import time
 import numpy as np
 import sympy as sym
 from numpy.linalg import linalg
+import os.path
 
 sym.init_printing()
 import math
@@ -32,7 +33,7 @@ pi_sym = sym.pi
 # robot = rtb.ERobot(e)
 # print(robot)
 # P = np.sin(np.linspace(-2.5,2.5))
-filename = 'point_cloud.csv'
+filename = 'q_pointcloud.csv'
 
 def Rx(q):
   T = np.array([[1,         0,          0, 0],
@@ -265,40 +266,7 @@ def plot_robots(rob_cnfs, traj_x, traj_y, traj_z):
 
         wd = 10  # Width
 
-        fig.add_scatter3d(
-            x=np.round(x_pos, 2),
-            y=np.round(y_pos, 2),
-            z=z_pos,
-            line=dict(color='darkblue', width=wd),
-            hoverinfo="none",  # hoverinfo="text",
-            hovertext=[f"joint {idx}: {q}"
-                       for idx, q in
-                       enumerate(np.round(np.rad2deg([0, q1, q2, q3, q4, q5]), 0))],
-            marker=dict(
-                size=wd / 2,
-                color=["black", "orange", "yellow", "pink", "blue", "goldenrod", "green", "red"],
-            ),
-            opacity=opc,
-            showlegend=False,
-            name=f"conf {i}"
-        )
 
-    fig.layout = dict(
-        width=1000,
-        height=700,
-        scene=dict(
-            camera=dict(eye={'x': -1.25, 'y': -1.25, 'z': 2}),
-            aspectratio={'x': 1.25, 'y': 1.25, 'z': 1},
-            # xaxis = dict( nticks=8, range=[np.min(traj_x)-0.5, np.max(traj_x)+0.5] ),
-            # yaxis = dict( nticks=8, range=[np.min(traj_y)-0.5, np.max(traj_y)+0.5] ),
-            # zaxis = dict( nticks=8, range=[-0.05, np.max(traj_z)+0.4] ),
-            xaxis_title='Robot x-axis',
-            yaxis_title='Robot y-axis',
-            zaxis_title='Robot z-axis'),
-        title=f"Robot plot in different configurations",
-        colorscale=dict(diverging="thermal")
-    )
-    pio.show(fig)
 
 
 def send(data, port=10003, addr='192.169.2.15'):
@@ -360,30 +328,7 @@ if __name__ == '__main__':
     scale_right = np.array([-0.085, -0.08789063, -0.08789063, -0.02, -0.085, -0.08789063, -0.08789063, 0.085, -0.085,
                             -0.085, 0.085, 1, -0.085])
     print(len(scale_left), len(scale_right))
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # sock.bind((UDP_IP, UDP_PORT))
-    # frame = []
-    # pa = pack(
-    #     'bbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhhbbhhhhhhh',
-    #     1, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, 3200, 3100,  # плечо левая
-    #     2, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, int(0 / 0.087 - 2088), int(11 / 0.087 - 2088),  # кисть
-    #     3, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, POSMIN, POSMAX,  # локоть
-    #     4, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, int(490 + 60 / 0.085), int(490 + 20 / 0.085),  # большой
-    #     5, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, int(2029 + 60 / 0.085), int(2029 + 70 / 0.085),  # указательный
-    #     6, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, int(1934 + 0 / -0.085), int(1934 + 60 / -0.085),  # средний
-    #     7, MODE, 456, 3000, CENTER, STIFF, 0, int(3000), int(3000),
-    #     # int((2478 + 60/0.085)),int(2478 + 10/0.085),#безымянный
-    #     8, MODE, ANGLE, 3000, CENTER, STIFF, 0, 3000, 3200,  # мизинец
-    #
-    #     1, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, POSMIN, POSMAX,  # правая
-    #     2, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, POSMIN, POSMAX,
-    #     3, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, POSMIN, POSMAX,
-    #     4, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, POSMIN, POSMAX,
-    #     5, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, POSMIN, POSMAX,
-    #     6, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, POSMIN, POSMAX,
-    #     7, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, POSMIN, POSMAX,
-    #     8, MODE, ANGLE, TORQUE, CENTER, STIFF, 0, 1616 + 550, 1616 + 700)
-    # send(pa)
+
 
     Kp = 4.8
     Ki = 0
@@ -399,7 +344,8 @@ if __name__ == '__main__':
     print(time.time())
     start = time.time()
 
-    while (time.time() - start < 120):
+    while (time.time() - start < 5):
+        print('*********',time.time() - start)
         # all this values in angels
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind((UDP_IP, UDP_PORT))
@@ -442,13 +388,12 @@ if __name__ == '__main__':
 
         q1, q2, q3, q4, q5 = math.radians(L_Shoulder), math.radians(L_Shoulder_S), math.radians(L_ElbowR_R),math.radians(L_Elbow), math.radians(L_WristR)
 
-        T01 = np.eye(4)@Ry(math.pi)
-        T12 = Rx(q1) @ Tx(a1)  # Joint 1 to 2
-        T23 = Ry(q2) #@ Tz(a2)  # Joint 2 to 3
-        T34 = Rz(q3) @ Tz(-a3)  # Joint 3 to 4
-        T45 = Rx(q4) @ Tz(-a4)  # Joint 4 to 5
-
-        T56 = Rz(q5) #@ Tz(a4)  # Joint 5 to 6
+        T01 = np.eye(4)
+        T12 = Ry(q1) @ Ty(a1)  # Joint 1 to 2
+        T23 = Rx(q2) @ Tz(a2)  # Joint 2 to 3
+        T34 = Rz(q3) @ Tz(a3)  # Joint 3 to 4
+        T45 = Ry(q4) @ Tz(a4)  # Joint 4 to 5
+        T56 = Rz(q5) @ Tz(a4)  # Joint 5 to 6
 
         T02 = T01 @ T12
         T03 = T01 @ T12 @ T23
@@ -464,13 +409,14 @@ if __name__ == '__main__':
         print('x = ', T06[0][3] ,'y = ', T06[1][3], 'z = ', T06[2][3])
         new_row = [q1, q2, q3, q4, q5]
         rows.append(new_row)
+        time.sleep(0.1)
+
         sock.close()
-        time.sleep(0.01)
     # writing to csv file
-    with open(filename+'1', 'w') as csvfile:
-    # creating a csv writer object
-        csvwriter = csv.writer(csvfile)
-        # writing the fields
-        csvwriter.writerow(fields)
-        # writing the data rows
-        csvwriter.writerows(rows)
+        with open(filename, 'a') as csvfile:
+        # creating a csv writer object
+            csvwriter = csv.writer(csvfile)
+            # writing the fields
+            #csvwriter.writerow(fields)
+            # writing the data rows
+            csvwriter.writerows(rows)
