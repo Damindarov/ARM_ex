0506e34065decd2e85d23e7ff9c198037f068bc6
Damindarov <damin4@mail.ru>
2022-01-07 14:36:44 +0300

    Remove a1,b1

--- cpp_pyth.py
+++ cpp_pyth.py
@@ -2,17 +2,18 @@ import socket
 import struct
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('192.168.0.116', 10000)
+sock.connect(server_address)
 
+# print(q4)
 # values = (q3, q1, q4, q5, q6)
 values = (15, 15, 15, 15, 15)
 
 packer = struct.Struct('f f f f f')
 packed_data = packer.pack(*values)
 while(True):
-    try:
-        sock.connect(server_address)
-        sock.sendall(packed_data)
+    # try:
+    sock.sendall(packed_data)
         # data1 = sock.recv(1024)
         # print(packer.unpack(data1))
-    finally:
-        sock.close()
+    # finally:
+    #     sock.close()
