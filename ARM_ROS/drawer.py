65c23937cfc524113bb5058f16360e039be7ba59
Damindarov <damin4@mail.ru>
2022-03-02 23:02:37 +0300

    Cleaned up the code, only the key parts remain

--- ARM_ROS/drawer.py
+++ ARM_ROS/drawer.py
@@ -1,10 +1,23 @@
-import csv
-file = open("data_point.csv")
-csvreader = csv.reader(file)
-header = next(csvreader)
-print(header)
-rows = []
-for row in csvreader:
-    rows.append(row)
-print(rows)
-file.close()
+# import csv
+# file = open("/home/r/PycharmProjects/ARM_ex/ARM_ROS/data_point.csv")
+# csvreader = csv.reader(file)
+# header = next(csvreader)
+# print(header)
+# rows = []
+# for row in csvreader:
+#     rows.append(row)
+# print(rows)
+# file.close()
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+headers = ['time', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10']
+
+df = pd.read_csv('/home/r/PycharmProjects/ARM_ex/ARM_ROS/data_point.csv', names=headers)
+
+df.set_index('time').plot()
+
+plt.show()
